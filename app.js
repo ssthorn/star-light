94279e2b604ee4c5db650a7e54e602dfcb5f4df0
Sam <sam.stearn@gmail.com>
2022-04-04 03:25:02 -0700

    op

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const cors = require('cors');
 const methodOverride = require("method-override");
 const session = require("express-session");
 const mongoose = require("mongoose");
-// const MongoDBStore = require("connect-mongodb-session")(session);
+const MongoDBStore = require("connect-mongodb-session")(session);
 const morgan = require('morgan');
-// const store = new MongoDBStore({uri: process.env.MONGO_URI,collection: "mySessions",});
+const store = new MongoDBStore({uri: process.env.MONGO_URI,collection: "mySessions",});
 require('./db-utils/connect');
 const taskController = require('./controllers/taskController');
 const userController = require('./controllers/userController');
 const { urlencoded } = require("express");
+
 app.use(urlencoded({extended: true}));
 app.use(cors())
 app.use(methodOverride('_method'))
@@ -19,7 +20,7 @@ app.use(morgan('short'))
 app.use(express.json());
 app.use('/tasks', taskController)
 app.use('/users', userController)
-app.use(mongoose);
-// app.use(session({secret: process.env.SESSION_SECRET,resave: false,saveUninitialized: false,store: store,}))
+
+app.use(session({secret: process.env.SESSION_SECRET,resave: false,saveUninitialized: false,store: store,}))
 const port = process.env.PORT || 3001;
 app.listen(port, () => {console.log("app running");});
